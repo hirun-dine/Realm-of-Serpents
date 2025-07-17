16eb8eccb2fe56e6846a920496281565b55ce5d0
hirun-dine <xx.spectrumdragon.xx@gmail.com>
2025-07-17 16:40:06 -0400

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -45,9 +45,10 @@ charadex.sheet = {
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
     rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Story', 'Collectors', 'NPC'],
+    mode: ['All', 'Story', 'Collectors', 'NPC'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
+    species: ['All', 'Human', 'Dragon', 'Familiar']
 
   }
 
